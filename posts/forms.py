2e04a74e22f5987ebfb0f264a92f97f5ee48a69e
PTK0225\erlle <erller123@gmail.com>
2022-03-03 15:32:47 +0900

    ddd

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-
-class PostForm(forms.Form):
-    title = forms.CharField(max_length=50, label='제목')
-    content = forms.CharField(label='내용', widget=forms.Textarea)
+from .models import Post
+class PostForm(forms.ModelForm):
     
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from .validators import validate_symbols
 
 # Create your models here.
 class Post(models.Model):
     # 글 제목, 내용, 작성일, 마지막 수정일
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    title = models.CharField(max_length=50,unique=True, error_messages={'unique': '이미 있는 제목입니다.'})
+    content = models.TextField(validators=[MinLengthValidator(10, '넘나 짧은거시와요 10자이상 적어주세요'), validate_symbols])
     dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
 
