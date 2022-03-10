c4563603cd6bd5b329ef739d15dcc62c9d12b1d6
PTK0225\erlle <erller123@gmail.com>
2022-03-10 21:48:10 +0900

    generic view update

--- posts/views.py
+++ posts/views.py
@@ -1,56 +1,42 @@
 from urllib import request
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
-from .forms import PostForm
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator
+from django.views.generic import (
+    CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
+)
+from django.urls import reverse
+from .models import Post
+from .forms import PostForm
+
+
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 6)
-    curr_page_number = request.GET.get('page')
-    if curr_page_number is None:
-        curr_page_number = 1
-    page = paginator.page(curr_page_number)
-    return render(request, 'posts/post_list.html',  {'page':page})
-    context = {"posts" : posts}
-    return render(request, 'posts/post_list.html', context)
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id) 
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
+class PostListView(ListView):
+    model = Post
+    ordering = ['-dt_created']
+    paginate_by = 6
+    
+class PostDetailView(DetailView):
+    model = Post
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk':self.object.id})
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
 
-    context = {"post" : post}
-    return render(request, 'posts/post_detail.html', context)
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk': self.object.id})
+        
+class PostDeleteView(DeleteView):
+    model = Post
 
-def post_create(request):
-    if request.method == 'POST':
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()        
-            return redirect('post-detail', post_id=new_post.id)
-    else:
-        post_form = PostForm()
-    return render(request, 'posts/post_form.html', {'form': post_form})
-
-def post_update(request, post_id):
-    post = get_object_or_404(Post, id=post_id) 
-    if request.method == 'POST':
-        post_form = PostForm(request.POST, instance=post)
-        if post_form.is_valid():
-            post_form.save()
-            return redirect('post-detail', post_id=post.id)
-    else:
-        post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form': post_form})
-
-def post_delete(request, post_id):
-    post = get_object_or_404(Post, id=post_id) 
-    if request.method == 'POST':
-        post.delete()
-        return redirect('post-list')
-    else:
-        return render(request, 'posts/post_confirm_delete.html', {'post': post})
-
-def index(request):
-    return redirect('post-list')
+    def get_success_url(self):
+        return reverse('post-list')
